08ef38b2f27ee171a588141dfcc3a251a832a61a
Unknown <saovalaksao@gmail.com>
2017-11-22 17:53:51 +0700

    ทำข้อ 1

--- lab_3_1.cpp
+++ lab_3_1.cpp
@@ -4,5 +4,12 @@ using namespace std;
 
 int main() {
   // เขียนโปรแกรมเพื่อแสดงผลตามที่กำหนด
+
+  cout << "Miss." << "Saovalak Saovakul" << endl ;
+  cout << "carrot" << endl << "female" << endl ;
+  cout << "26/2/1999" << endl << "read a book" << endl ;
+  cout << "single";
+
+
   return 0;
 }
